a9386b9bead1562fa1ab814f012572929e617618
elvis.he <elvis.he@gaiaworks.cn>
2020-02-22 21:46:37 +0800

    导入apache加密工具包

--- src/main/java/com/hezhan/wx/util/EncryptionUtils.java
+++ src/main/java/com/hezhan/wx/util/EncryptionUtils.java
@@ -1,12 +1,10 @@
 package com.hezhan.wx.util;
 
 import com.hezhan.wx.exception.CustomException;
-import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.stereotype.Component;
 
-import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 /**
  * @Author Zhanzhan
@@ -27,12 +25,35 @@ public class EncryptionUtils {
         try {
             MessageDigest md = MessageDigest.getInstance(type);
             byte[] digest = md.digest(data.getBytes());
-            return byteToString(digest);
+            return Hex.encodeHexString(digest);
         } catch (Exception e) {
             throw new CustomException("加密时报错");
         }
     }
 
+    /**
+     * 将加密后的byte数组转为String
+     * @param data
+     * @return
+     */
+    private String byteArrayToString(byte[] data){
+        StringBuffer sb = new StringBuffer(data.length * 2);
+        String temp = null;
+        for (byte b : data){
+            temp = Integer.toHexString(b & 0xFF);
+            if (temp.length() == 1){
+                sb.append("0");
+            }
+            sb.append(temp);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 将加密后的byte数组转为String
+     * @param data
+     * @return
+     */
     private String byteToString(byte[] data) {
         char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                 'a', 'b', 'c', 'd', 'e', 'f'};
